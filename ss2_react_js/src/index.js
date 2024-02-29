fab893d9826a7af40b3a41a1d1365fbcc954ccb2
phamthanhx18 <138504962+phamthanhx18@users.noreply.github.com>
2024-02-29 09:12:21 +0700

    git

--- ss2_react_js/src/index.js
+++ ss2_react_js/src/index.js
@@ -10,7 +10,7 @@ import "bootstrap/dist/css/bootstrap.css";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <SignUp />
+    <Display />
   </React.StrictMode>
 );
 
